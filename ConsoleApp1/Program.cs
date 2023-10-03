e2b07162da11035f4fcaf8db06ca3232d3fff681
adam.dedek <adam.dedek@PC-LAB05>
2023-10-03 16:06:31 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[,] grid = new char[n, n];
+        char[,] row = new char[n, n];
+
+        char[, ] clear(char[,] table)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    table[i, j] = '-';
+                }
+            }
+            return table;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            string m = Console.ReadLine();
+            for (int j = 0; j < n; j++)
+            {
+                grid[i, j] = m[j];
+            }
+        }
+
+        int[, ] function(int a,int b,int k) 
+        {
+            int test = 0;
+
+            if ((int)grid[a-1, b] == k++)
+            {
+                function(a - 1, b, k++);
+                row[a - 1, b] = (char)(k++);
+            }
+            if ((int)grid[a + 1, b] == k++)
+            {
+                function(a + 1, b, k++);
+                test++;
+            }
+            if ((int)grid[a, b - 1] == k++)
+            {
+                function(a, b - 1, k++);
+                test++;
+            }
+            if ((int)grid[a, b + 1] == k++)
+            {
+                function(a, b + 1, k++);
+                test++;
+            }
+            if (test >= 2)
+            {
+                return [a, b];
+            }
+
+            return [a,b];
+        }
+
+        for (int x = 0; x < n; x++)
+        {
+            for (int y = 0; y < n; y++)
+            {
+                if (grid[x, y] == 'a')
+                {
+                    row[x, y] = 'a';
+                    function(x, y, 98);
+                }
+            }
+        }
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("answer");
+    }
+}
---
+++ ConsoleApp1/death.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int N = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+        int L = int.Parse(inputs[1]); // the number of links
+        int E = int.Parse(inputs[2]); // the number of exit gateways
+        int[,] links = new int[N, N];
+        int[] gateways = new int[E];
+
+        for (int i = 0; i < L; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int N1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+            int N2 = int.Parse(inputs[1]);
+            links[i, i] = links[N1, N2];
+        }
+        for (int i = 0; i < E; i++)
+        {
+            int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
+            gateways[i] = gateways[EI];
+        }
+        /*int[, ] links = new int[L, L];
+        for (int i = 0; i < L; i++)
+        {
+
+        }*/
+        // game loop
+        while (true)
+        {
+            int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Bobnet agent is positioned this turn
+            
+            for (int i = 0; i < L; i++)
+            {
+                if (links[i, i] == links[gateways[i], SI])
+                {
+                    if (gateways[i] > SI)
+                    {
+                        Console.WriteLine(SI + " " + gateways[i]);
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine(gateways[i] + " " + SI);
+                        return;
+                    }
+                }
+                else if (links[gateways[i], SI] == links[i, i])
+                {
+                    if (gateways[i] > SI)
+                    {
+                        Console.WriteLine(SI + " " + gateways[i]);
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine(gateways[i] + " " + SI);
+                        return;
+                    }
+                }
+            }
+            Random rnd = new Random();
+
+            for (int i = 0; i < L; i++)
+            {
+
+            }
+        }
+    }
+}
